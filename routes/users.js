d411c53821dc3303f7de9136e82166baf679a593
BaptistePayan <teck-bat@hotmail.fr>
2025-04-23 18:46:20 +0200

    création users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const service = require('../services/users');
+const private = require('../middlewares/privates');
+const userService = require('../services/loginFromForm');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/addUsers', (req, res) => {
+  res.render('users/addUsers')
+})
+
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard'); // 
 });
+
+router.get('/:id', private.checkJWT, service.getUserById);
+
+router.post('/', service.add);
+
+router.patch('/:id', private.checkJWT, service.update);
+
+router.delete('/:id', service.delete);
+
+router.post('/authenticate', service.authenticate);
+
+router.post('/login-form', userService.loginFromForm);
+
 
 module.exports = router;
