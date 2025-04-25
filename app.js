db86a866bd42fd5c7ab9cec5cdfb2a73298734f9
BaptistePayan <teck-bat@hotmail.fr>
2025-04-25 17:28:16 +0200

    installation de modules de dépendance et modification de app.js

--- app.js
+++ app.js
@@ -20,11 +20,48 @@ mongodb.initClientDbConnection();
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+//moteur ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Configuration du store MongoDB pour les sessions
+const store = new MongoDBStore({
+    uri: process.env.URL_MONGO, // URL de connexion à MongoDB
+    collection: 'sessions' // Collection pour stocker les sessions
+});
+
+store.on('error', function(error) {
+    console.error("Erreur avec MongoDBStore :", error);
+});
+
+
+// Utilise method-override pour remplacer la méthode POST par DELETE ou PUT
+app.use(methodOverride('_method')); // '_method' correspond au nom du champ caché dans le formulaire
+
+// Middleware pour gérer la session
+app.use(session({
+    secret: process.env.SECRET_KEY || 'clé_secrète', // Clé secrète pour sécuriser les sessions
+    resave: false,  // Ne pas resauvegarder la session si elle n'a pas été modifiée
+    saveUninitialized: false,  // Ne pas créer une session pour les visiteurs non authentifiés
+    store: store, // Utiliser MongoDBStore
+    cookie: {
+        httpOnly: true,  // Sécuriser le cookie pour qu'il ne soit pas accessible via JavaScript
+        secure: process.env.NODE_ENV === 'production', // Utiliser un cookie sécurisé en production
+        maxAge: 60 * 60 * 1000  // Durée de vie du cookie (1 heure ici)
+    }
+}));
+
+// Middleware
+app.use(cors({
+    exposedHeaders: ['Authorization'],
+    origin: '*'
+}));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
