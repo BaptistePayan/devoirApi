991e1c42f22840c818c56d5bfd6fd2c770290ffe
BaptistePayan <teck-bat@hotmail.fr>
2025-04-28 12:53:41 +0200

    création des routes et des CRUD

---
+++ services/catways.js
@@ -0,0 +1,79 @@
+const catways = require('../models/catways');
+
+exports.getCatwayById = async (req, res) => {
+    try {
+        const catway = await catways.findById(req.params.id);
+        if (!catway) {
+            return res.status(404).json({ message: 'Catway non trouvé' });
+        }
+        res.status(200).json(catway);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
+exports.add = async (req, res) => {
+    const { catwayNumber, catwayType, catwayState } = req.body;
+    try {
+        const catway = new catways({ catwayNumber, catwayType, catwayState });
+        await catway.save();
+        res.render('catways/addCatways', { catways: catway });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.update = async (req, res) => {
+    const { catwayState } = req.body;
+    try {
+        const updatedCatway = await catways.findByIdAndUpdate(
+            req.params.id,
+            { catwayState },
+            { new: true }
+        );
+        if (!updatedCatway) {
+            return res.status(404).json({ message: 'Catway non trouvé' });
+        }
+        res.redirect('/catways'); 
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.delete = async (req, res) => {  
+    try {
+        const catway = await catways.findByIdAndDelete(req.params.id);
+        if (!catway) {
+            return res.status(404).json({ message: 'Catway non trouvé' });
+        }
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+exports.getAllCatways = async (req, res) => {
+    try {
+        const catwaysList = await catways.find();
+        res.render('catways/listCatways', { catways: catwaysList });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.showEditForm = async (req, res) => {
+    try {
+        const catway = await catways.findById(req.params.id);
+        if (!catway) {
+            return res.status(404).json({ message: 'Catway non trouvé' });
+        }
+        res.render('catways/editCatways', { catway }); 
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+exports.showAddForm = (req, res) => {
+    res.render('catways/addCatways');
+};
---
+++ services/reservations.js
@@ -0,0 +1,75 @@
+const reservations = require('../models/reservations');
+
+exports.getReservationById = async (req, res) => {
+    try {
+        const reservation = await reservations.findById(req.params.id);
+        if (!reservation) {
+            return res.status(404).json({ message: 'Réservation non trouvée' });
+        }
+        res.status(200).json(reservation);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}   
+
+exports.add = async (req, res) => {
+    const { catwayNumber, clientName, boatName, startDate, endDate } = req.body;
+    try {
+        const reservation = new reservations({ catwayNumber, clientName, boatName, startDate, endDate });
+        await reservation.save();
+        res.render('reservations/addReservations', { reservation });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+exports.update = async (req, res) => {  
+    const { catwayNumber, clientName, boatName, startDate, endDate } = req.body;
+    try {
+        const updatedReservation = await reservations.findByIdAndUpdate(
+            req.params.id,
+            { catwayNumber, clientName, boatName, startDate, endDate },
+            { new: true }
+        );
+        if (!updatedReservation) {
+            return res.status(404).json({ message: 'Réservation non trouvée' });
+        }
+        res.render('reservations/editReservation', { reservation: updatedReservation });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}   
+exports.delete = async (req, res) => {
+    try {
+        const reservation = await reservations.findByIdAndDelete(req.params.id);
+        console.log(reservation);  
+        console.log(id);
+        if (!reservation) {
+            return res.status(404).json({ message: 'Réservation non trouvée' });
+        }
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+exports.getAllReservations = async (req, res) => {
+    try {
+        const reservationsList = await reservations.find();
+        res.render('reservations/list', { reservations: reservationsList });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+exports.showUpdateReservationForm = async (req, res) => {   
+    try {
+        const reservation = await reservations.findById(req.params.id);
+        if (!reservation) {
+            return res.status(404).json({ message: 'Réservation non trouvée' });
+        }
+        res.render('reservations/editReservation', { reservation });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
