796f3238294abc2b671ffd34e014a441d0640c68
BaptistePayan <teck-bat@hotmail.fr>
2025-05-19 19:03:52 +0200

    test

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('API is running')
 })
+app.get('/test', (req, res) => {
+    res.send('ok test')
+})
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 //moteur ejs
